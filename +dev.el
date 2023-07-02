9021a435f867e748223c72e5afa6d8150cfa6725
Niv Sardi <xaiki@evilgiggle.com>
2023-07-02 20:47:02 -0300

    kill paredit-RET
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- +dev.el
+++ +dev.el
@@ -25,6 +25,7 @@
   (setq paredit-indent-after-open nil)
   (define-key paredit-mode-map (kbd "C-<left>") nil)
   (define-key paredit-mode-map (kbd "C-<right>") nil)
+  (define-key paredit-mode-map (kbd "RET") nil)
 
   (require 'eldoc) ; if not already loaded
   (eldoc-add-command
